e344f4fefc4ac0c528688fe11d94fa1b6be414e2
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 05:19:52 +0000

    add nav bar

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Nav = () => {
+    return (
+        <div className='nav'>
+            <h1>Reddit<span className='highlight'>Minimal</span></h1>
+            <img src='/reddit-logo.png' alt='reddit logo'/>
+        </div>
+    );
+};
+
+export default Nav;
