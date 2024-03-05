eb8dfb7a1ed09080310939a441bd7d160fc7fb6c
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 08:47:14 +0000

    import image

--- src/components/Vote/Vote.js
+++ src/components/Vote/Vote.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import '../../styles/Vote.css';
+import arrowUpImg from '../../assets/arrow-up.png';
+import arrowDownImg from '../../assets/down-arrow.png';
 
 const Vote = (props) => {
     return (
         <div className='votes'>
             <div className='upvotes'>
-                <img src='/arrow-up.png' alt='arrow up'/>
+                <img src={arrowUpImg} alt='arrow up'/>
             </div>
             {props.score}
             <div className='downvotes'>
-                <img src='/down-arrow.png' alt='arrow down'/>
+                <img src={arrowDownImg} alt='arrow down'/>
             </div>
         </div>
     );
