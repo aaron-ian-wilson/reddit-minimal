8d340aa63f3ef6024258b1fc005b4a96f8c0a6ae
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 22:35:23 +0000

    adding a temporary 'under construction' header for early deployment

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -4,9 +4,15 @@ import redditImg from '../../assets/reddit-logo.png';
 
 const Nav = () => {
     return (
+        <div>
+
+        <div className='temp'>
+            <h4>Currenty under construction</h4>
+        </div>
         <div className='nav'>
             <h1>Reddit<span className='highlight'>Minimal</span></h1>
             <img src={redditImg} alt='reddit logo'/>
+        </div>
         </div>
     );
 };
