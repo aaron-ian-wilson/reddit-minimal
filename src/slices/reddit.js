71421b943679f558b57c0dcbbe313285843d642c
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 06:04:37 +0000

    add reddit slice

---
+++ src/slices/reddit.js
@@ -0,0 +1,12 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const reddit = createSlice({
+    name: reddit,
+    state: {
+        posts: [],
+        comments: [],
+        status: 'idle',
+    },
+    reducers: {},
+    
+})
