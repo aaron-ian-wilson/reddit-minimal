b9d37817b74931e6e86c87f7eafb185ea9ab3c80
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 23:30:00 +0000

    removing unused variables for netlify

--- src/components/Topics/Topics.js
+++ src/components/Topics/Topics.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState } from 'react';
 import '../../styles/Topics.css';
 import { NavLink, createSearchParams, useNavigate } from 'react-router-dom';
 import popularImg from '../../assets/popular.png';
