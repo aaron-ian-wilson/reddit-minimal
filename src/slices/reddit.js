e153a9d8f1301339212238fc73d043bc049562fd
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 06:19:14 +0000

    add extra reducers and selectors

--- src/slices/reddit.js
+++ src/slices/reddit.js
@@ -6,7 +6,39 @@ const reddit = createSlice({
         posts: [],
         comments: [],
         status: 'idle',
+        error: false,
     },
     reducers: {},
-    
-})
+    extraReducers: (builder) => {
+        builder
+          .addMatcher(
+            (action) => action.type.endsWith('/pending'),
+            (state) => {
+              state.status = 'pending';
+              state.error = false;
+            }
+          )
+          .addMatcher(
+            (action) => action.type.endsWith('/fulfilled'),
+            (state, action) => {
+              state.status = 'fulfilled';
+              state.posts = action.payload;
+              state.comments = action.payload;
+              state.error = false
+            }
+          )
+          .addMatcher(
+            (action) => action.type.endsWith('/rejected'),
+            (state) => {
+              state.status = 'rejected';
+              state.error = true;
+            }
+          );
+      },
+});
+
+export default reddit.reducer;
+
+export const selectPosts = (state) => state.reddit.posts;
+export const selectComments = (state) => state.reddit.comments;
+export const selectStatus = (state) => state.reddit.status;
