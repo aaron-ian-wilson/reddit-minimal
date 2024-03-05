339393bc90bdd6d419811ff9e35d9ed9b7a59555
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 05:35:40 +0000

    add provider

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
 import './styles/global.css';
 import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    
     <App />
   </React.StrictMode>
 );
