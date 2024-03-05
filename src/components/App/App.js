c4c2d3ce4550e7844b6797eceb86afdd05084823
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 06:33:56 +0000

    import Home and add it as the index path

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,10 @@
 import Root from '../../routes/Root';
+import Home from '../../routes/Home';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={ <Root/> }>
-        
+        <Route index element={ <Home/> }/>
     </Route>
 ));
 
