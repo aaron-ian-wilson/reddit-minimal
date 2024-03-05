323875907057da337036c4dbdcc9cc28a30c7834
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 06:31:10 +0000

    add jsx

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,2 +1,18 @@
 import React from 'react';
+import '../styles/Home.css';
 
+const Home = () => {
+    return (
+        <div className='body'>
+            <div className='sidebar-left'>
+
+            </div>
+            <div className='content'>
+
+            </div>
+            <div className='sidebar-right'>
+
+            </div>
+        </div>
+    )
+}
