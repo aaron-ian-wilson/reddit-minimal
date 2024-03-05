235d37daa716301b5fe5986c0202496d0d271742
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 07:57:53 +0000

    add topics route with effects

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,14 @@
 import Root from '../../routes/Root';
 import Home from '../../routes/Home';
 import Post from '../../routes/Post';
+import Search from '../../routes/Search';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={ <Root/> }>
         <Route index element={ <Home/> }/>
         <Route path='/post/:id' element={ <Post/> }/>
+        <Route path='/search' element={ <Search/> }/>
     </Route>
 ));
 
---
+++ src/routes/Topics.js
@@ -0,0 +1,38 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import Topics from '../components/Topics/Topics';
+import Search from '../components/Search/Search';
+import Card from '../components/Card/Card';
+import { useSearchParams } from 'react-router-dom';
+import { selectPosts } from '../slices/reddit';
+import { topics } from '../actions/topics';
+
+const Topics = () => {
+    const post = useSelector(selectPosts);
+
+    const [searchParams] = useSearchParams();
+    const query = searchParams.get('name');
+
+    const dispatch = useDispatch();
+    
+    useEffect(() => {
+        dispatch(topics(query));
+    }, [dispatch]);
+
+    return (
+        <div className="body">
+        <div className="sidebar-left">
+            <Topics />
+        </div>
+        <div className="content">
+            <Search />
+            {post.map((data, index) => <Card key={index} data={data}/>)}
+        </div>
+        <div className="sidebar-right">
+            
+        </div>
+    </div>
+    );
+};
+
+export default Topics;
