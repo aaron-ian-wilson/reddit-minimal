f50406bd42069aed6416195683ce2fb1617ed7fb
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 05:34:07 +0000

    add routing

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,7 +1,16 @@
-import './App.css';
+import Root from '../../routes/Root';
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
+
+const router = createBrowserRouter(createRoutesFromElements(
+    <Route path='/' element={ <Root/> }>
+
+    </Route>
+));
 
 function App() {
-
-}
+    return (
+        <RouterProvider router={router}/>
+    );
+};
 
 export default App;
