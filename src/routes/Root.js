d901c18786fff5113c40e9f8c585f23424690354
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 05:29:28 +0000

    add jsx

--- src/routes/Root.js
+++ src/routes/Root.js
@@ -1,7 +1,23 @@
 import React from "react";
+import Nav from "../components/Nav/Nav";
 
 const Root = () => {
-    
+    return (
+        <>
+        <Nav/>
+        <div className="body">
+            <div className="sidebar-left">
+
+            </div>
+            <div className="content">
+                
+            </div>
+            <div className="sidebar-right">
+
+            </div>
+        </div>
+        </>
+    )
 };
 
 export default Root;
