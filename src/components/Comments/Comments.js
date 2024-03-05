c7a6822c685feefa294b5f1cb1833861a1317b6b
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 07:31:45 +0000

    add comments with handlers

---
+++ src/components/Comments/Comments.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const Comments = ({ comment }) => {
+    const [commentCount, setCommentCount] = useState(1);
+
+    const showMore = () => {
+        setCommentCount(commentCount + 1);
+    }
+
+    const showLess = () => {
+        setCommentCount(commentCount - 1);
+    }
+
+    return (
+        <div className='comment-list'>
+            {comment.slice(0, commentCount).map((comment, index) => (
+                <div key={index}>
+                    <span className='highlight'>{comment.author}</span>
+                    <p>{comment.body}</p>
+                    {comment.replies.map((reply, index) => (
+                        <div key={index} className='reply'>
+                            <span className='highlight'>{reply.author}</span>
+                            <p>{reply.body}</p>
+                        </div>
+                    ))}
+                </div>
+            ))}
+            <div className='buttons-container'>
+                {commentCount >= 1 && (
+                    <button onClick={showMore}><img src='/plus.png'/></button>
+                )}
+                {commentCount >= 2 && (
+                    <button onClick={showLess}><img src='/minus.png'/></button>
+                )}
+            </div>
+    </div>
+    );
+};
+
+export default Comments;
