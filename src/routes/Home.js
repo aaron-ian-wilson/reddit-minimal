2b10e5b3e535fed5c5453642043a7b93d24bf8de
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 07:23:28 +0000

    imported topics and render topics

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -2,12 +2,13 @@ import React from 'react';
 import '../styles/Home.css';
 import Search from '../components/Search/Search';
 import CardList from '../components/CardList/CardList';
+import Topics from '../components/Topics/Topics';
 
 const Home = () => {
     return (
         <div className='body'>
             <div className='sidebar-left'>
-
+                <Topics/>
             </div>
             <div className='content'>
                 <Search/>
