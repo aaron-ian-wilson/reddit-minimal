a34850cbc91644b009f139d506c6ba42396dc39a
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 06:56:55 +0000

    add effects and jsx

---
+++ src/components/CardList/CardList.js
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Card } from '../Card/Card';
+import { selectPosts } from '../../slices/reddit';
+import { popular } from '../../actions/popular-posts';
+
+export const CardList = () => {
+    const posts = useSelector(selectPosts);
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch(popular()); // Fetch popular posts from reddits API
+    }, [dispatch]);
+
+    return (
+        <div className='card-list'>
+            {posts.map(data => <Card key={data.id} data={data}/>)}
+        </div>
+    );
+};
+
