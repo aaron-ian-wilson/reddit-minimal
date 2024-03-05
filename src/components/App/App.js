693679bb9f5b0327d39d344957b67111868ee985
aaron <aaron.ian.wilson@gmail.com>
2024-03-05 07:47:16 +0000

    import Post and add route

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,12 @@
 import Root from '../../routes/Root';
 import Home from '../../routes/Home';
+import Post from '../../routes/Post';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 
 const router = createBrowserRouter(createRoutesFromElements(
     <Route path='/' element={ <Root/> }>
         <Route index element={ <Home/> }/>
+        <Route path='/post/:id' element={ <Post/> }/>
     </Route>
 ));
 
